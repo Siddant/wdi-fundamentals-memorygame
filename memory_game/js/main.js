c9901f10089e45a77e1d4625d0af13c893ac7457
siddant <siddantgurung@hotmail.co.uk>
2018-11-15 20:04:18 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,24 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-
-if(cardsInPlay.length === 2){
+var checkForMatch = function(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
-
 	}
 }
+
+var flipCard = function(cardId){
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
+
+	if(cardsInPlay.length === 2){
+		checkForMatch();
+	}
+
+}
+
+
+flipCard(0);
+flipCard(2);
